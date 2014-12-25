f2ba898822ec69ddadc18ba38996dba9c2f2c3b3
anntiantian <anntiantian@163.com>
2014-12-25 17:05:57 +0800

    Add the hole

--- gravityPlatform.js
+++ gravityPlatform.js
@@ -10,10 +10,10 @@ define(function (require, exports) {
     var cvs = document.getElementById("canvas");
     var ctx = cvs.getContext('2d');
     var img = document.createElement("img");
-    img.src = "./ball.png";
     img.onload = function(){
         $("#start").removeAttr("disabled");
-    }
+    };
+    img.src = "./ball.png";
     var minX = 0;
     var minY = 0;
     var maxX = cvs.width - 20;
@@ -28,27 +28,54 @@ define(function (require, exports) {
         this.vx = 0;  // velocity x
         this.vy = 0;  // velocity y
         this.timer = null;
+        this.hole = {x: 0, y: 0};
     }
 
     Ball.prototype = { 
         init: function(){
             var that = this;
+            this.createHole();
             ctx.drawImage( img, this.x, this.y );
             this.timer = window.setInterval(function(){that.move();}, 25);
         },
         move: function(){
+            this.clearMyself();
             var x = this.x + xg * (-1); //x轴重力加速度于x轴相反
             var y = (this.y + yg);
             this.x = Math.max( Math.min( x, maxX ), minX );
             this.y = Math.max( Math.min( y, maxY ), minY );
+            if( this.x > this.hole.x - 20 && this.x < this.hole.x 
+                && this.y > this.hole.y - 20 && this.y < this.hole.y ){
+                $("#start").trigger("click");
+            }
+            this.drawHole();
             this.draw();
+            
+        },
+        clearMyself: function(){
+            ctx.fillStyle = "#FFFFFF";//使用颜色值为白色，透明为0的颜色填充
+            ctx.beginPath();
+            ctx.arc(this.x + 10, this.y + 10, 11, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fill();
         },
         stop: function(){
             window.clearInterval( this.timer );
         },
         draw: function(){
-            ctx.clearRect(0, 0, cvs.width, cvs.height);
             ctx.drawImage( img, this.x, this.y );
+        },
+        createHole: function(){
+            this.hole.x = parseInt(Math.random()*(maxX - minX + 1) + minX+10); 
+            this.hole.y = parseInt(Math.random()*(maxY - minY + 1) + minY+10); 
+            this.drawHole();
+        },
+        drawHole: function(){
+            ctx.fillStyle="#FF0000";
+            ctx.beginPath();
+            ctx.arc(this.hole.x,this.hole.y,20,0,Math.PI*2,true);
+            ctx.closePath();
+            ctx.fill();
         }
     }
     
@@ -74,6 +101,8 @@ define(function (require, exports) {
     
     $("#start").on("click", function(){
         if( running == false ){
+            cvs.width = cvs.width;
+            cvs.height = cvs.height;
             ball = new Ball();
             ball.init();
             running = true;
@@ -88,5 +117,10 @@ define(function (require, exports) {
     $("#test").on("click", function(){
         ball = new Ball();
         ball.init();
+        running = true;
+        window.setInterval(function(){
+            xg = Math.random() * -10;
+            yg = Math.random() * 10;
+        }, 25);
     })
 })
